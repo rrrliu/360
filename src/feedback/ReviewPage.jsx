b9a28ea60cda1c9ecb85508d9c98ac2a5cd27642
Richard <richardliuwa@gmail.com>
2018-10-14 19:36:18 -0700

    added submit button

--- src/feedback/ReviewPage.jsx
+++ src/feedback/ReviewPage.jsx
@@ -7,6 +7,7 @@ export default class ReviewPage extends Component {
             <div>
                 <Feedback text='[REDACTED] was the worst person I have ever met in my eighteen years of life.' />
                 <Feedback text='[REDACTED] was a great teammate and always got the job done.' />
+				<input type="submit" value="Submit"/>
             </div>
         )
     }
