139e3af80c521b1ed633b3cafc36695a28c12b91
Richard <richardliuwa@gmail.com>
2018-10-14 18:25:07 -0700

    added routing, cleaned up the content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GivePage from './feedback/GivePage';
 import ReviewPage from './feedback/ReviewPage';
 import './App.css';
@@ -8,20 +8,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Switch>
+            <Route exact path="/" />
+            <Route exact path="/give" component={GivePage} />
+            <Route exact path="/review" component={ReviewPage} />
+          </Switch>
+        </Router>
       </div>
     );
   }
