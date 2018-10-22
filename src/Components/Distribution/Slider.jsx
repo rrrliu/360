6e653e8f1289805063308329608ec2d2c76d9a66
Harish Palani <harishpalani@berkeley.edu>
2018-10-21 18:22:43 -0700

    pls work

--- src/Components/Distribution/Slider.jsx
+++ src/Components/Distribution/Slider.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export default class Slider extends Component {
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        step: props.initialStep,
+        count: props.initialCount
+      };
+    }    
+
+    increment() {
+        this.setState({count: this.state.count + this.state.step});
+    }
     
+    decrement() {
+        this.setState({count: this.state.count - this.state.step});
+    }
+  
+    update(e) {
+        this.setState({
+            step: parseInt(e.target.value, 10) || 0
+        })
+    }
+  
+    reset(e) {
+        this.setState({count: 0});
+    }
+
     render() {
+        const { count, step } = this.state
         return (
             <div className="member">
-                <Result result={this.props.count} />
+                <Result result={this.state.count} />
                 <Button onClick={() => this.decrement()}>-</Button>        
                 <Button onClick={() => this.increment()}>+</Button>
             </div>
@@ -13,6 +41,16 @@ export default class Slider extends Component {
     }
     
 }
+
+// Properties
+Slider.propTypes = {
+    initialCount: PropTypes.number
+};
+
+Slider.defaultProps = {
+    initialStep: 5,
+    initialCount: 0
+};
 
 // Components
 const Result = ({result}) => (
